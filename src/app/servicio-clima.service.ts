d205771fa14430ed68e0436b7902f1186c55e4c4
StanPalma <bladimirpalma4@gmail.com>
2022-09-24 19:07:01 -0600

    throwError

--- src/app/servicio-clima.service.ts
+++ src/app/servicio-clima.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +10,16 @@ export class ServicioClima {
   // baseUrl = 'http://api.weatherunlocked.com/api/current/';
   // baseUrl = 'http://api.weatherapi.com/v1/current.json?key=dfca66bbcac244cdbfc235416222409&q=el salvador&aqi=no';
   baseUrl = 'http://api.weatherapi.com/v1/current.json?key=dfca66bbcac244cdbfc235416222409&q=';
-  positionUrl = 'https://api.api-ninjas.com/v1/city?name=';
 
   constructor(private http: HttpClient) { }
-
-  
-
-  getLocation(ciudad:string) {
-    /* let headers = new HttpHeaders();
-    headers = headers.set('X-Api-Key', '1eO+WxdGpKkLicWDTi9MRQ==Tl40FNDnzQTGmqCV'); */
-    
-    this.positionUrl = `${this.positionUrl}` + ciudad;
-    //console.log(this.positionUrl);
-    // return this.http.get(this.positionUrl, {headers: headers});
-  }
 
   getClima(ciudad:string): Observable<any> {
     /* let result = this.http.get(this.baseUrl);
     console.log(result); */
     // return this.http.get(this.baseUrl + position + "?app_id=392fd6be&app_key=93a8f3d21539eda6a180de46b4804b9d");
-    return this.http.get(`${this.baseUrl}` + ciudad + `&aqi=no`);
+    return this.http.get(`${this.baseUrl}` + ciudad + `&aqi=no`).pipe(map(data => {
+      if (data === null) return throwError("null data");
+      else return data;
+    }));
   }
 }
--- src/app/servicio-convert.service.ts
+++ src/app/servicio-convert.service.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServicioConvertService {
-
   baseUrl = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getValue() {
     /* let result = this.http.get(this.baseUrl);
     console.log(result); */
-    return this.http.get(this.baseUrl);
+    return this.http.get(this.baseUrl).pipe(
+      map((data) => {
+        if (data === null) return throwError('null data');
+        else return data;
+      })
+    );
   }
 }
